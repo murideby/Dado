760c89b539d440c47005a5331912e47cef6a223f
Débora Murielle <debora.murielle@gmail.com>
2021-09-03 20:57:33 -0300

    Criando rolar dado

--- app/src/main/java/com/example/dado/MainActivity.kt
+++ app/src/main/java/com/example/dado/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.dado
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            val toast = Toast.makeText(this, "Dado rolado!", Toast.LENGTH_SHORT)
+            val resultTextView: TextView = findViewById(R.id.textView)
+            resultTextView.text = "6"
+            toast.show()
+                rollDice()
+        }
+
     }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+
+    }
+
+    class Dice(private val numSides: Int) {
+
+        fun roll(): Int {
+            return (1..numSides).random()
+        }
+    }
+
+
 }
